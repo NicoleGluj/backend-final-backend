ece0215d8c07bb2afdd5b7db3222dda5826c1be5
NicoleGluj <nicolegluj@gmail.com>
2025-12-07 14:52:27 -0300

    add: connectDB en index

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express from "express"
+import cors from "cors"
+import { connectDB } from "./config/mongodb"
+
+process.loadEnvFile()
+
+const { PORT, URI_DB } = process.env
+
+if (!PORT || !URI_DB) {
+  console.error("❌ Error: Variables de entorno faltantes")
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      // user?: IUserTokenPayload
+    }
+  }
+}
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+// app.use(logger)
+
+app.listen(PORT, () => {
+  console.log(`✅ Servidor en escucha en el puerto ${PORT}`)
+  connectDB()
+})
