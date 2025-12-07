443f782dbc297a6ccd1b690bfa5fda097da97e12
NicoleGluj <nicolegluj@gmail.com>
2025-12-07 14:41:55 -0300

    add: connectDB

---
+++ src/config/index.ts
@@ -0,0 +1,23 @@
+import mongoose = require("mongoose")
+
+process.loadEnvFile()
+
+export const connectDB = async () => {
+  const uri = process.env.URI_DB
+
+  if (!uri) {
+    console.log("❌Error: No se encontro URI_DB en las variables de entorno")
+    process.exit(1)
+  }
+
+  try {
+    console.log("🧩 Intentando conectar con MongoDB...")
+    await mongoose.connect(uri)
+    console.log("✅  Conectado exitosamente con MongoDB")
+  } catch (error: any) {
+    console.error("❌Error al conectarse con MongoDB")
+    console.error(error.message)
+    process.exit(1)
+  }
+}
+
